4781ea6b26740a3fa2a620028d21cc1cc87d45ee
unknown <lang0909@ajou.ac.kr>
2021-05-01 17:24:02 +0900

    add test login code

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Login(){
     const [id, setId] = useState<string>('')
     const [pw, setPw] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
     },[])
 
+    async function handleLogin(id : string, password : string){
+        setLoading(true)
+        try{
+            const res = await axios.post('http://183.98.69.129/auth/members/signin', {
+                memberId: id,
+                password: password
+            })
+            if(res.status === 200){
+                alert('로그인성공')
+                console.log(res.data)
+            }
+        }catch(error){
+            throw error
+        }finally{
+            setLoading(false)
+        }
+    }
+
     return(
-        <>
+        <div style={ loading ? { backgroundColor: 'rgba(0, 0, 0, 0.5)', opacity: 0 } : {}}>
             Login Page
             <div>
                 <span>ID: </span>
@@ -27,8 +47,8 @@ function Login(){
                     onChange={(e) => setPw(e.target.value)}
                 />
             </div>
-            <button>로그인</button>
-        </>
+            <button onClick={() => handleLogin(id, pw)}>로그인</button>
+        </div>
     )
 }
 
